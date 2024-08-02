27e7d9c5b5486dcfae02272cc2c8e3e783066fc8
Chad Birch <chadbirch@hotmail.com>
2024-08-02 11:42:39 -0400

    Moved to name ranges

---
+++ .github/workflows/full-band-input-list-workflow/full-band-input-list-workflow.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import excel2img
+import win32com.client
+from pywintypes import com_error
+
+EXCEL_FILE_PATH = r'Input List/Full Band/Input List.xlsx'
+
+INPUT_EQUIPMENT_NAME_RANGE = r'InputEquipmnetExportRange'
+PATH_TO_INPUT_EQUIPMENT_PNG = r'Input List/Full Band/Input + Equipment.png'
+
+PATH_TO_PDF = r'Input List/Full Band/Input List.pdf'
+
+excel2img.export_img(EXCEL_FILE_PATH, PATH_TO_INPUT_EQUIPMENT_PNG, "", INPUT_EQUIPMENT_NAME_RANGE)
+
+excel = win32com.client.Dispatch("Excel.Application")
+excel.Visible = False
+try:
+    print('Start conversion to PDF')
+    # Open
+    wb = excel.Workbooks.Open(EXCEL_FILE_PATH)
+    # Specify the sheet you want to save by index. 1 is the first (leftmost) sheet.
+    ws_index_list = [1,2]
+    wb.WorkSheets(ws_index_list).Select()
+    # Save
+    wb.ActiveSheet.ExportAsFixedFormat(0, PATH_TO_PDF)
+except com_error as e:
+    print('failed.')
+else:
+    print('Succeeded.')
+finally:
+    wb.Close()
+    excel.Quit()
