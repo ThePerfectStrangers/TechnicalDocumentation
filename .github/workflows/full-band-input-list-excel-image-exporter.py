68054252937a8b78bb3ed74a80b650d3295abb1f
Chad Birch <chadbirch@hotmail.com>
2024-08-02 02:14:02 -0400

    Test change

--- .github/workflows/full-band-input-list-excel-image-exporter.py
+++ .github/workflows/full-band-input-list-excel-image-exporter.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import excel2img
 
 excel2img.export_img("../../Input List/Full Band/Input List.xlsx", "../../Input List/Full Band/Input + Equipment.png", "Input + Equipment", None)
