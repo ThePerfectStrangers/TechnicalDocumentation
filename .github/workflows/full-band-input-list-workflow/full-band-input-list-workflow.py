15effdd5d217d1b433563dfbee516f756dc17c13
Chad Birch <chadbirch@hotmail.com>
2024-09-09 01:10:10 -0400

    Test

--- .github/workflows/full-band-input-list-workflow/full-band-input-list-workflow.py
+++ .github/workflows/full-band-input-list-workflow/full-band-input-list-workflow.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import win32com.client
 import excel2img
 
 EXCEL_FILE_PATH = 'Input List/Full Band/Input List.xlsx'
@@ -16,3 +17,14 @@ except:
     print('Image Export Failed!')
 else:
     print('Image Export Succeeded!')
+
+try:
+    o = win32com.client.Dispatch("Excel.Application")
+    o.Visible = False
+    wb = o.Workbooks.Open(EXCEL_FILE_PATH)
+    wb.ExportAsFixedFormat(0, PATH_TO_PDF)
+    wb.Close(True)
+except:
+    print('PDF Export Failed!')
+else:
+    print('PDF Export Succeeded!')
