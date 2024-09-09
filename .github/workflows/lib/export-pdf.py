fcd1cb76395f75861c01571c8a41c3e6fdd98cdf
Chad Birch <chadbirch@hotmail.com>
2024-09-09 11:50:04 -0400

    Test

--- .github/workflows/lib/export-pdf.py
+++ .github/workflows/lib/export-pdf.py
@@ -7,21 +7,15 @@ import os
 EXCEL_FILE_PATH = sys.argv[1]
 PDF_FILE_PATH = sys.argv[2]
 
-EXCEL_FILE_ABSOLUTE_PATH = os.path.abspath(EXCEL_FILE_PATH)
-PDF_FILE_ABSOLUTE_PATH = os.path.abspath(PDF_FILE_PATH)
-
 print(f"Excel File Path: {EXCEL_FILE_PATH}")
 print(f"PDF File Path: {PDF_FILE_PATH}")
-
-print(f"Absolute Excel File Path: {EXCEL_FILE_ABSOLUTE_PATH}")
-print(f"Absolute PDF File Path: {PDF_FILE_ABSOLUTE_PATH}")
 
 try:
     o = win32com.client.Dispatch("Excel.Application")
     o.Visible = False
 
-    wb = o.Workbooks.Open(EXCEL_FILE_ABSOLUTE_PATH)
-    wb.ExportAsFixedFormat(0, PDF_FILE_ABSOLUTE_PATH)
+    wb = o.Workbooks.Open(EXCEL_FILE_PATH)
+    wb.ExportAsFixedFormat(0, PDF_FILE_PATH)
 
     wb.Close(True)
 except Exception as e:
--- .github/workflows/lib/export-png.py
+++ .github/workflows/lib/export-png.py
@@ -2,14 +2,10 @@
 
 import sys
 import win32com.client
-import os
 from PIL import ImageGrab
 
 EXCEL_FILE_PATH = sys.argv[1]
 OUTPUT_PNG_FILE_PATH = sys.argv[2]
-
-ABSOLUTE_EXCEL_FILE_PATH = os.path.abspath(EXCEL_FILE_PATH)
-ABSOLUTE_OUTPUT_PNG_FILE_PATH = os.path.abspath(OUTPUT_PNG_FILE_PATH)
 
 WORKSHEET = sys.argv[3]
 RANGE = sys.argv[4]
@@ -19,13 +15,13 @@ print(f"Output PNG File Path: {OUTPUT_PNG_FILE_PATH}")
 print(f"WORKSHEET: {WORKSHEET}")
 print(f"RANGE: {RANGE}")
 
-print(f"Absolute Excel File Path: {ABSOLUTE_EXCEL_FILE_PATH}")
+print(f"Absolute Excel File Path: {EXCEL_FILE_PATH}")
 print(f"Absolute PNG File Path: {OUTPUT_PNG_FILE_PATH}")
 
 try:
     o = win32com.client.Dispatch("Excel.Application")
     o.Visible = False
-    wb = o.Workbooks.Open(ABSOLUTE_EXCEL_FILE_PATH)
+    wb = o.Workbooks.Open(EXCEL_FILE_PATH)
     worksheet = wb.Worksheets(WORKSHEET)
     used_range = worksheet.UsedRange
     used_range.CopyPicture(Format=2)
@@ -37,7 +33,7 @@ try:
         print("No image found in clipboard.")
     else:
         # Save the image temporarily
-        img.save(ABSOLUTE_OUTPUT_PNG_FILE_PATH, 'PNG')
+        img.save(OUTPUT_PNG_FILE_PATH, 'PNG')
 
 except Exception as e:
     print('Image Export Failed!')
