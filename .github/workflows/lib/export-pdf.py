c9e45e96ebe550c66a979f96f9ef316786135c81
Chad Birch <chadbirch@hotmail.com>
2024-09-09 02:29:26 -0400

    Test

--- .github/workflows/lib/export-pdf.py
+++ .github/workflows/lib/export-pdf.py
@@ -10,6 +10,12 @@ PDF_FILE_PATH = sys.argv[2]
 EXCEL_FILE_ABSOLUTE_PATH = os.path.abspath(EXCEL_FILE_PATH)
 PDF_FILE_ABSOLUTE_PATH = os.path.abspath(PDF_FILE_PATH)
 
+print(f"Excel File Path: {EXCEL_FILE_PATH}")
+print(f"PDF File Path: {PDF_FILE_PATH}")
+
+print(f"Absolute Excel File Path: {EXCEL_FILE_ABSOLUTE_PATH}")
+print(f"Absolute PDF File Path: {PDF_FILE_ABSOLUTE_PATH}")
+
 try:
     o = win32com.client.Dispatch("Excel.Application")
     o.Visible = False
--- .github/workflows/lib/export-png.py
+++ .github/workflows/lib/export-png.py
@@ -6,19 +6,27 @@ import os
 from PIL import ImageGrab
 
 EXCEL_FILE_PATH = sys.argv[1]
-INPUT_EQUIPMENT_PNG_PATH = sys.argv[2]
+OUTPUT_PNG_FILE_PATH = sys.argv[2]
 
-EXCEL_FILE_ABSOLUTE_PATH = os.path.abspath(EXCEL_FILE_PATH)
-INPUT_EQUIPMENT_PNG_ABSOLUTE_PATH = os.path.abspath(INPUT_EQUIPMENT_PNG_PATH)
+ABSOLUTE_EXCEL_FILE_PATH = os.path.abspath(EXCEL_FILE_PATH)
+ABSOLUTE_OUTPUT_PNG_FILE_PATH = os.path.abspath(OUTPUT_PNG_FILE_PATH)
 
-INPUT_EQUIPMENT_SHEET = sys.argv[3]
-INPUT_EQUIPMENT_RANGE = sys.argv[4]
+WORKSHEET = sys.argv[3]
+RANGE = sys.argv[4]
+
+print(f"Excel File Path: {EXCEL_FILE_PATH}")
+print(f"Output PNG File Path: {OUTPUT_PNG_FILE_PATH}")
+print(f"WORKSHEET: {WORKSHEET}")
+print(f"RANGE: {RANGE}")
+
+print(f"Absolute Excel File Path: {ABSOLUTE_EXCEL_FILE_PATH}")
+print(f"Absolute PNG File Path: {OUTPUT_PNG_FILE_PATH}")
 
 try:
     o = win32com.client.Dispatch("Excel.Application")
     o.Visible = False
-    wb = o.Workbooks.Open(EXCEL_FILE_ABSOLUTE_PATH)
-    worksheet = wb.Worksheets(INPUT_EQUIPMENT_SHEET)
+    wb = o.Workbooks.Open(ABSOLUTE_EXCEL_FILE_PATH)
+    worksheet = wb.Worksheets(WORKSHEET)
     used_range = worksheet.UsedRange
     used_range.CopyPicture(Format=2)
 
@@ -29,7 +37,7 @@ try:
         print("No image found in clipboard.")
     else:
         # Save the image temporarily
-        img.save(INPUT_EQUIPMENT_PNG_ABSOLUTE_PATH, 'PNG')
+        img.save(ABSOLUTE_OUTPUT_PNG_FILE_PATH, 'PNG')
 
 except Exception as e:
     print('Image Export Failed!')
